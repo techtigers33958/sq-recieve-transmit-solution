0b73e1f68e5b33e0146154e68b383b48c491e9f2
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-08 10:49:56 -0400

    Innovated Solution

--- main.ts
+++ main.ts
@@ -3,16 +3,22 @@
 // -Calculate Answer and send it
 radio.onReceivedValue(function (name, value) {
     calculateAnswer(name, value)
-    radio.sendString("" + (answer))
+})
+// When you tilt right, show the question
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showString(question)
 })
 // Set the question to the received string
 radio.onReceivedString(function (receivedString) {
     question = receivedString
 })
-// When B is pressed show the answer (for debugging
-// purposes)
-input.onButtonPressed(Button.B, function () {
+// When A and B are pressed, show the string
+input.onButtonPressed(Button.AB, function () {
     basic.showString("" + (answer))
+})
+// When B is pressed- reset
+input.onButtonPressed(Button.B, function () {
+    answer = 0
 })
 // This calculates the answer based on the received
 // operation and number
@@ -27,11 +33,11 @@ function calculateAnswer (operation: string, operand: number) {
         answer = operand - 1
     }
 }
-// When button A is pressed show the question (for
-// debugging purposes)
+// When A is pressed send the string
 input.onButtonPressed(Button.A, function () {
-    basic.showString(question)
+    radio.sendString("" + (answer))
 })
-let question = ""
 let answer = 0
+let question = ""
 radio.setGroup(50)
+basic.showIcon(IconNames.Yes)
